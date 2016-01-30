7a2eb05fad17c842c6471afa2f500082ca44cf38
Adrian Unger <dev@staydecent.ca>
2016-01-30 12:44:31 -0800

    Add Base model and new colour palette.

--- app/src/main/java/apps/staydecent/com/tabletoptally/models/Game.java
+++ app/src/main/java/apps/staydecent/com/tabletoptally/models/Game.java
@@ -1,24 +1,11 @@
 package apps.staydecent.com.tabletoptally.models;
 
 import io.realm.RealmList;
-import io.realm.RealmObject;
-import io.realm.annotations.PrimaryKey;
 
-public class Game extends RealmObject {
-
-    @PrimaryKey
-    private long id;
+public class Game extends Base {
 
     private String name;
     private RealmList<Score> scores;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
 
     public String getName() {
         return name;
--- app/src/main/java/apps/staydecent/com/tabletoptally/models/Score.java
+++ app/src/main/java/apps/staydecent/com/tabletoptally/models/Score.java
@@ -1,29 +1,12 @@
 package apps.staydecent.com.tabletoptally.models;
 
-import com.google.common.base.Joiner;
-import com.google.common.base.Splitter;
-
-import io.realm.RealmObject;
-import io.realm.annotations.PrimaryKey;
-
-public class Score extends RealmObject {
+public class Score extends Base {
 
     private static final String DELIM = "; ";
-
-    @PrimaryKey
-    private long id;
 
     private String players;
     private String winner;
     private Game game;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
 
     public String getPlayers() {
         return players;
